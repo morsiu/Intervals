30b84b53ab79889d38a06cac5fd45886a8190db8
Łukasz Mrozek <morskk@gmail.com>
2013-12-05 21:54:05 +0100

    Using NUnit as testing framework.

--- Walrus.Ranges.Test/RangeEqualityComparerTests.cs
+++ Walrus.Ranges.Test/RangeEqualityComparerTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Walrus.Ranges.Test
 {
-    [TestClass]
+    [TestFixture]
     public class RangeEqualityComparerTests
     {
     }
