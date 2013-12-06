9a906d5c60c692da7bde06e09999fd2f2a3e3e06
Łukasz Mrozek <morskk@gmail.com>
2013-12-06 18:29:14 +0100

    Added empty range.

---
+++ Walrus.Ranges.Test/EmptyRangeTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Walrus.Ranges.Test
+{
+    public class EmptyRangeTests
+    {
+        [TestFixture]
+        public class AfterConstruction
+        {
+            private EmptyRange<int> emptyRange;
+
+            [TestFixtureSetUp]
+            public void SetUp()
+            {
+                emptyRange = new EmptyRange<int>();
+            }
+
+            [Test]
+            public void ShouldBeEmpty()
+            {
+                Assert.IsTrue(emptyRange.IsEmpty);
+            }
+
+            [Test]
+            public void StartShouldThrowInvalidOperationException()
+            {
+                Assert.Throws<InvalidOperationException>(
+                    () => { var foo = emptyRange.Start; });
+            }
+
+            [Test]
+            public void EndShouldThrowInvalidOperationException()
+            {
+                Assert.Throws<InvalidOperationException>(
+                    () => { var foo = emptyRange.End; });
+            }
+
+            [Test]
+            public void HasOpenStartShouldThrowInvalidOperationException()
+            {
+                Assert.Throws<InvalidOperationException>(
+                    () => { var foo = emptyRange.HasOpenStart; });
+            }
+
+            [Test]
+            public void HasOpenEndShouldThrowInvalidOperationException()
+            {
+                Assert.Throws<InvalidOperationException>(
+                    () => { var foo = emptyRange.HasOpenEnd; });
+            }
+        }
+    }
+}
---
+++ Walrus.Ranges/EmptyRange.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Walrus.Ranges
+{
+    internal sealed class EmptyRange<T> : IRange<T>
+        where T : IComparable<T>
+    {
+        public bool IsEmpty
+        {
+            get { return true; }
+        }
+
+        public T Start
+        {
+            get { throw new InvalidOperationException(); }
+        }
+
+        public T End
+        {
+            get { throw new InvalidOperationException(); }
+        }
+
+        public bool HasOpenStart
+        {
+            get { throw new InvalidOperationException(); }
+        }
+
+        public bool HasOpenEnd
+        {
+            get { throw new InvalidOperationException(); }
+        }
+
+        public bool Equals(IRange<T> other)
+        {
+            return RangeEqualityComparer<T>.Instance.Equals(this, other);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return RangeEqualityComparer<T>.Instance.Equals(this, obj as IRange<T>);
+        }
+
+        public override int GetHashCode()
+        {
+            return RangeEqualityComparer<T>.Instance.GetHashCode(this);
+        }
+    }
+}
