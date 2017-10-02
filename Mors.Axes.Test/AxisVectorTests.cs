808e0556403629f5d2ec2576967737041fa116ec
Łukasz Mrozek <morskk@gmail.com>
2017-10-02 20:14:30 +0200

    Added tests for AxisVector.Left

---
+++ Mors.Axes.Test/AxisVectorTests.cs
@@ -0,0 +1,17 @@
+﻿using Mors.Ranges.Test.Support.RangeOperations;
+using NUnit.Framework;
+
+namespace Mors.Axes.Test
+{
+    public static class AxisVectorTests
+    {
+        [Test]
+        [TestCase(-10, true)]
+        [TestCase(0, false)]
+        [TestCase(10, false)]
+        public static void LeftIsTrueForNegativeMangitudeAndDirection(int magnitudeAndDirection, bool expectedLeft)
+        {
+            Assert.That(new AxisVector(magnitudeAndDirection).Left(), Is.EqualTo(expectedLeft));
+        }
+    }
+}
