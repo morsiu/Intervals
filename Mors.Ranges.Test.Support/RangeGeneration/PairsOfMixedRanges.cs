78f5664807199eca27913c80e8f56a7023f93cc5
Łukasz Mrozek <morskk@gmail.com>
2018-05-20 09:32:05 +0200

    Extracted PairsOfRangesOfKinds.GenerateMixedPairs to PairsOfMixedRanges

--- Mors.Ranges.Test.Support/RangeGeneration/PairsOfMixedRanges.cs
+++ Mors.Ranges.Test.Support/RangeGeneration/PairsOfMixedRanges.cs
@@ -1,32 +1,33 @@
-﻿// Copyright (C) 2013 Łukasz Mrozek
+﻿// Copyright (C) 2018 Łukasz Mrozek
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace Mors.Ranges.Test.Support.RangeGeneration
 {
-    public sealed class PairsOfRangesOfKinds : IEnumerable<RangePair>
+    internal sealed class PairsOfMixedRanges : IEnumerable<RangePair>
     {
-        private readonly IEnumerable<Tuple<RangeKind, RangeKind>> _pairsOfRangeKinds;
+        private readonly RangeKind _rangeAKind;
+        private readonly RangeKind _rangeBKind;
 
-        public PairsOfRangesOfKinds(IEnumerable<Tuple<RangeKind, RangeKind>> pairsOfRangeKinds)
+        public PairsOfMixedRanges(RangeKind rangeAKind, RangeKind rangeBKind)
         {
-            _pairsOfRangeKinds = pairsOfRangeKinds;
+            _rangeAKind = rangeAKind;
+            _rangeBKind = rangeBKind;
         }
 
         public IEnumerator<RangePair> GetEnumerator()
         {
-            foreach (var pairOfRangeKinds in _pairsOfRangeKinds)
+            var aRanges = new RangesOfAKind(_rangeAKind);
+            foreach (var rangeA in aRanges)
             {
-                var rangeAKind = pairOfRangeKinds.Item1;
-                var rangeBKind = pairOfRangeKinds.Item2;
-                foreach (var pair in GeneratePairs(rangeAKind, rangeBKind))
+                var bRanges = new RangesOfAKind(_rangeBKind);
+                foreach (var rangeB in bRanges)
                 {
-                    yield return pair;
+                    yield return new RangePair(rangeA, rangeB);
                 }
             }
         }
@@ -34,13 +35,6 @@ namespace Mors.Ranges.Test.Support.RangeGeneration
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
-        }
-
-        private static IEnumerable<RangePair> GeneratePairs(RangeKind rangeAKind, RangeKind rangeBKind)
-        {
-            if (rangeAKind == RangeKind.NonEmpty && rangeBKind == RangeKind.NonEmpty)
-                return new PairsOfNonEmptyRanges();
-            return new PairsOfMixedRanges(rangeAKind, rangeBKind);
         }
     }
 }
