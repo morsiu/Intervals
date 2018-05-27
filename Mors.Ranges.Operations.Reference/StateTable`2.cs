8e43e712ca77f194dfdb85325c120cb90e4e444d
Łukasz Mrozek <morskk@gmail.com>
2018-05-27 16:22:55 +0200

    StateTable includes the input value in the exception for missing corresponding output

--- Mors.Ranges.Operations.Reference/StateTable`2.cs
+++ Mors.Ranges.Operations.Reference/StateTable`2.cs
@@ -1,8 +1,9 @@
-﻿// Copyright (C) 2013 Łukasz Mrozek
+﻿// Copyright (C) 2018 Łukasz Mrozek
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+using System;
 using System.Collections.Generic;
 
 namespace Mors.Ranges.Operations.Reference
@@ -20,8 +21,9 @@ namespace Mors.Ranges.Operations.Reference
 
         public TOutput Match(TInput input)
         {
-            var output = _states[input];
-            return output;
+            return _states.TryGetValue(input, out var output)
+                ? output
+                : throw new ArgumentException($"There is no defined output for input {input}");
         }
     }
 }
