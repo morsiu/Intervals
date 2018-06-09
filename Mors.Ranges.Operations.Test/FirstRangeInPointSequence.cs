3cdf919e5bc92c5830a3aa29cd5293a48310fea3
Łukasz Mrozek <morskk@gmail.com>
2018-06-09 17:35:14 +0200

    Added FirstRangeInPointSequence

---
+++ Mors.Ranges.Operations.Test/FirstRangeInPointSequence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Mors.Ranges.Sequences;
+
+namespace Mors.Ranges
+{
+    internal sealed class FirstRangeInPointSequence : IRange<int>
+    {
+        private readonly Lazy<IRange<int>> _range;
+
+        public FirstRangeInPointSequence(IPointSequence pointSequence)
+        {
+            _range = new Lazy<IRange<int>>(
+                () => new RangesInPointSequence(pointSequence)
+                    .Select(x => Range.Create<int>(x.Start, x.End, x.HasOpenStart, x.HasOpenEnd))
+                    .DefaultIfEmpty(Range.Empty<int>())
+                    .SingleOrDefault());
+        }
+
+        public bool Equals(IRange<int> other) => RangeEqualityComparer<int>.Instance.Equals(this, other);
+
+        public bool IsEmpty => _range.Value.IsEmpty;
+        public int Start => _range.Value.Start;
+        public int End => _range.Value.End;
+        public bool HasOpenStart => _range.Value.HasOpenStart;
+        public bool HasOpenEnd => _range.Value.HasOpenEnd;
+    }
+}
