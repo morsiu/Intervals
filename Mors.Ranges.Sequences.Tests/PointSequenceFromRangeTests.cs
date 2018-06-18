0e5492451cc67f8b2e2a3a71af9d4fd7142946fe
Łukasz Mrozek <morskk@gmail.com>
2018-06-18 16:29:44 +0200

    Decoupled PointSequenceFromRangeTests from Range structure

--- Mors.Ranges.Sequences.Tests/PointSequenceFromRangeTests.cs
+++ Mors.Ranges.Sequences.Tests/PointSequenceFromRangeTests.cs
@@ -12,26 +12,26 @@ namespace Mors.Ranges.Sequences.Tests
     {
         public static IEnumerable<TestCaseData> EnumerationTestCases()
         {
-            yield return new TestCaseData(new Range(0, 0, true, true)).Returns(new TestablePointSequence("-", 0));
-            yield return new TestCaseData(new Range(0, 0, false, true)).Returns(new TestablePointSequence("-", 0));
-            yield return new TestCaseData(new Range(0, 0, true, false)).Returns(new TestablePointSequence("-", 0));
-            yield return new TestCaseData(new Range(0, 0, false, false)).Returns(new TestablePointSequence("#", 0));
-            yield return new TestCaseData(new Range(0, 1, false, false)).Returns(new TestablePointSequence("[]", 0));
-            yield return new TestCaseData(new Range(0, 1, true, false)).Returns(new TestablePointSequence("(]", 0));
-            yield return new TestCaseData(new Range(0, 1, false, true)).Returns(new TestablePointSequence("[)", 0));
-            yield return new TestCaseData(new Range(0, 1, true, true)).Returns(new TestablePointSequence("()", 0));
-            yield return new TestCaseData(new Range(0, 2, false, false)).Returns(new TestablePointSequence("[=]", 0));
-            yield return new TestCaseData(new Range(0, 2, true, false)).Returns(new TestablePointSequence("(=]", 0));
-            yield return new TestCaseData(new Range(0, 2, false, true)).Returns(new TestablePointSequence("[=)", 0));
-            yield return new TestCaseData(new Range(0, 2, true, true)).Returns(new TestablePointSequence("(=)", 0));
+            yield return new TestCaseData((0, 0, true, true)).Returns(new TestablePointSequence("-", 0));
+            yield return new TestCaseData((0, 0, false, true)).Returns(new TestablePointSequence("-", 0));
+            yield return new TestCaseData((0, 0, true, false)).Returns(new TestablePointSequence("-", 0));
+            yield return new TestCaseData((0, 0, false, false)).Returns(new TestablePointSequence("#", 0));
+            yield return new TestCaseData((0, 1, false, false)).Returns(new TestablePointSequence("[]", 0));
+            yield return new TestCaseData((0, 1, true, false)).Returns(new TestablePointSequence("(]", 0));
+            yield return new TestCaseData((0, 1, false, true)).Returns(new TestablePointSequence("[)", 0));
+            yield return new TestCaseData((0, 1, true, true)).Returns(new TestablePointSequence("()", 0));
+            yield return new TestCaseData((0, 2, false, false)).Returns(new TestablePointSequence("[=]", 0));
+            yield return new TestCaseData((0, 2, true, false)).Returns(new TestablePointSequence("(=]", 0));
+            yield return new TestCaseData((0, 2, false, true)).Returns(new TestablePointSequence("[=)", 0));
+            yield return new TestCaseData((0, 2, true, true)).Returns(new TestablePointSequence("(=)", 0));
         }
 
         [Test]
         [TestCaseSource(nameof(EnumerationTestCases))]
-        public TestablePointSequence EnumerationTest(in Range input)
+        public TestablePointSequence EnumerationTest(in (int Start, int End, bool StartOpen, bool EndOpen) input)
         {
             return new TestablePointSequence(
-                new PointSequenceFromRange(input.Start, input.End, input.HasOpenStart, input.HasOpenEnd));
+                new PointSequenceFromRange(input.Start, input.End, input.StartOpen, input.EndOpen));
         }
     }
 }
