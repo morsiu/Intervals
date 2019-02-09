15facff950aa7697aaefe4af067c4a331a143f85
Łukasz Mrozek <morskk@gmail.com>
2019-02-09 09:52:37 +0100

    Remove unused types in generation library

--- Mors.Ranges.Generation/IRanges.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mors.Ranges.Generation
-{
-    public interface IRanges<out TRange>
-    {
-        TRange Empty();
-        TRange Range(int start, int end, bool isStartOpen, bool isEndOpen);
-    }
-}
