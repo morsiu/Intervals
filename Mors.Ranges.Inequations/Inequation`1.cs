42e3318949dedcd972051aa96410cd791dd5d126
Łukasz Mrozek <morskk@gmail.com>
2019-10-17 17:46:54 +0200

    Replace property used privately with a field

--- Mors.Ranges.Inequations/Inequation`1.cs
+++ Mors.Ranges.Inequations/Inequation`1.cs
@@ -6,45 +6,45 @@ namespace Mors.Ranges.Inequations
     public readonly struct Inequation<T> : IEquatable<Inequation<T>>
         where T : IComparable<T>
     {
-        internal Inequation(IInequation<T> value) => Value = value;
+        private readonly IInequation<T> _value;
 
-        internal IInequation<T> Value { get; }
+        internal Inequation(IInequation<T> value) => _value = value;
 
         public Inequation<T> And(Inequation<T> other) =>
-            new Inequation<T>(new And<T>(Value, other.Value));
+            new Inequation<T>(new And<T>(_value, other._value));
 
         public Inequation<T> Closure() =>
-            new Inequation<T>(new Closure<T>(Value));
+            new Inequation<T>(new Closure<T>(_value));
 
         public Inequation<T> Not() =>
-            new Inequation<T>(new Not<T>(Value));
+            new Inequation<T>(new Not<T>(_value));
 
         public Inequation<T> Or(Inequation<T> other) =>
-            new Inequation<T>(new Or<T>(Value, other.Value));
+            new Inequation<T>(new Or<T>(_value, other._value));
 
         public bool IsSatisfiedBy(T point) =>
-            Value.IsSatisfiedBy(point);
+            _value.IsSatisfiedBy(point);
 
         public bool Equals(Inequation<T> other) =>
-            Value.Equals(other.Value);
+            _value.Equals(other._value);
 
         public override bool Equals(object obj) =>
             obj is Inequation<T> other && Equals(other);
 
         public override int GetHashCode() =>
-            Value.GetHashCode();
+            _value.GetHashCode();
 
         public IEnumerable<TRange> ToClosedRanges<TRange, TPoints, TRanges>()
             where TRanges : struct, IClosedRanges<T, TRange>
             where TPoints : struct, IPoints<T> =>
-            new ClosedRangesInInequation<T, TRange, TPoints, TRanges>(Value);
+            new ClosedRangesInInequation<T, TRange, TPoints, TRanges>(_value);
 
         public IEnumerable<TRange> ToOpenRanges<TRange, TPoints, TRanges>()
             where TRanges : struct, IOpenRanges<T, TRange>
             where TPoints : struct, IPoints<T> =>
-            new OpenRangesInInequation<T, TRange, TPoints, TRanges>(Value);
+            new OpenRangesInInequation<T, TRange, TPoints, TRanges>(_value);
 
         public override string ToString() =>
-            Value.ToString();
+            _value.ToString();
     }
 }
